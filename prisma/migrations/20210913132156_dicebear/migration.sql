1cbe639ea68f595c6532d046850e7f3384018763
Dhruv Roy Talukdar <dhruvroy8@gmail.com>
2021-09-13 18:52:29 +0530

    dicebear integration

---
+++ prisma/migrations/20210913132156_dicebear/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Made the column `photoURL` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "photoURL" SET NOT NULL,
+ALTER COLUMN "photoURL" DROP DEFAULT;
